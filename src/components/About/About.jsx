101679d825f994403912425092a369b07d4594cc
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-11 11:57:26 +0600

    set the Outlet

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div>
+<h3>About us</h3>            
+<p>Everything you want to know.</p>
+        </div>
+    );
+};
+
+export default About;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h2>This is Home component</h2>
+      <Outlet></Outlet>
     </div>
   );
 };
