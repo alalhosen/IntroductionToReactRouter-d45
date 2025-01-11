93022f6e734e227160a7229fd1f5e8e1e377c6ba
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-11 13:26:10 +0600

    add Link  in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
-
+import { Link } from 'react-router-dom';
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <h2>Navber</h2>
             <nav>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact Us</a>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact Us</Link>
             </nav>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 const Home = () => {
   return (
     <div>
+    <Header></Header>
       <h2>This is Home component</h2>
       <Outlet></Outlet>
     </div>
