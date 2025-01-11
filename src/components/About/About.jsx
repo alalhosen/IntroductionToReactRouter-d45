f77af06d0b6e350ca6886a1e8c7233b60318dc4e
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-11 16:55:30 +0600

    some component add

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,10 @@
-
-
 const About = () => {
-    return (
-        <div>
-<h3>About us</h3>            
-<p>Everything you want to know.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>About us</h2>
+      <p>Everything you want to know.</p>
+    </div>
+  );
 };
 
 export default About;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,11 @@
+
+const Users = () => {
+  return (
+    <div>
+      <h3>Users section</h3>
+      <p>Use the Users</p>
+    </div>
+  );
+};
+
+export default Users;
