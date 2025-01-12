515a48de665099bd0cdf846cccb09c870c37e4e9
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-12 22:55:54 +0600

    create user component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,12 @@
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
+  const users = useLoaderData();
+
+  console.log(users)
   return (
     <div>
-      <h3>Users section</h3>
+      <h2>Our Users: {users.length}</h2>
       <p>Use the Users</p>
     </div>
   );
