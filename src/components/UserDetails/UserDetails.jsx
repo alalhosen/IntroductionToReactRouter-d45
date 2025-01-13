16113be5b57068071a80f2e5798761932c4e4cee
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-14 00:28:49 +0600

    create btn beside the Show Details Link

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,7 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
 const UserDetails = () => {
+
+    const user= useLoaderData();
+    const {name, email, username}=user;
   return (
     <div>
-      <h2>Details about user</h2>
+      <h2>Details about user:{name}</h2>
+      <p>email:{email}</p>
+      <p>username:{username}</p>
     </div>
   );
 };
