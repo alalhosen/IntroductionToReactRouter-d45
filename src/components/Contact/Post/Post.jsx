72c28264af61e12a75da7473df65643c069ea942
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-14 23:36:57 +0600

    css add in post component

--- src/components/Contact/Post/Post.jsx
+++ src/components/Contact/Post/Post.jsx
@@ -1,8 +1,16 @@
 
-const Post = () => {
+const Post = ({post}) => {
+    const {id, title}=post;
+
+    const postStyle = {
+        border: "2px solid lime",
+        padding: "5px",
+        borderRadius: "20px",
+      };
     return (
-        <div>
-            
+        <div style={postStyle}>
+            <h2>Post of id:{id}</h2>
+            <p>title:{title}</p>
         </div>
     );
 };
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,16 @@
 import { useLoaderData } from "react-router-dom";
+import Post from "../Contact/Post/Post";
 
 const Posts = () => {
     const posts= useLoaderData();
   return (
     <div>
       <h2>posts: {posts.length} </h2>
+      <div className="users">
+        {
+            posts.map(post=><Post key={post.id} post={post}></Post>)
+        }
+      </div>
     </div>
   );
 };
