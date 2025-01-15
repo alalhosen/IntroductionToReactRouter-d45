5ad38efbe819fbd1c2ebe1182ef06f2e897283c5
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-16 00:00:39 +0600

    error not found page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,20 @@
-
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-<h2>Oopa!!!</h2>            
-        </div>
-    );
+    const error = useRouteError();
+    console.log(error)
+  return (
+    <div>
+      <h2>Oopa!!!</h2>
+      <p>{error.statusText || error.message}</p>
+      {
+        error.status === 404 && <div>
+            <h3>Page not found</h3>
+            
+            </div>
+      }
+    </div>
+  );
 };
 
 export default ErrorPage;
