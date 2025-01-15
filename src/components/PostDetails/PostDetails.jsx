6bb6044edf8b9240318419c8bf86a5082e517d4f
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-15 23:36:05 +0600

    Go Back btn create in post details component

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,14 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
-    const post=useLoaderData();
-    const {id, title, body} =post;
+  const post = useLoaderData();
+
+  const navigate = useNavigate()
+  const { id, title, body } = post;
+
+  const handleGoBack=()=>{
+navigate(-1);
+  }
+
   return (
     <div>
       <h2>Post details about: {id}</h2>
       <p>Title: {title}</p>
-      <p><small>{body}</small></p>
-
+      <p>
+        <small>{body}</small>
+      </p>
+      <button onClick={handleGoBack}>Go Back</button>
     </div>
   );
 };
