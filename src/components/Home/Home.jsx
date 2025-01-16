f0df8856cf8936adea8f71a310ce017366a50045
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-16 23:59:33 +0600

    navigation & location add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Home = () => {
+  const navigation=useNavigation();
+  const location=useLocation();
   return (
     <div>
     <Header></Header>
