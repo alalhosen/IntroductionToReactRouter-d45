01d7928a37397289b770ae1d711b281e25188413
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-17 23:04:38 +0600

    change NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
-import { Link, NavLink } from 'react-router-dom';
-import './Header.css'
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div>
-            <nav>
-                <span>My Website</span>
-                <Link to='/'>Home</Link>
-                <NavLink to='/users'>Users</NavLink>
-                <NavLink to='/posts'>Posts</NavLink>
-                <Link to='/about'>About</Link>
-                <Link to='/contact'>Contact Us</Link>
-            </nav>
-        </div>
-    );
+  return (
+    <div>
+      <nav>
+        <span>My Website</span>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+        {/* <NavLink to="/about">About</NavLink> */}
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact Us</NavLink>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
