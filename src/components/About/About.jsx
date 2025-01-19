cde9ad693a02622f8baca01966e0d338d5e2d28b
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-19 22:08:19 +0600

    add lorem & btn Home

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <div>
       <h2>About us</h2>
       <p>Everything you want to know.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos eligendi aliquam, earum in impedit porro voluptatum laudantium excepturi, suscipit odio iure ipsum perferendis. Dolore veniam est molestias accusamus, aliquam neque.
+      Mollitia corporis deserunt autem quae illo dolorem voluptatibus consectetur ullam reprehenderit quisquam expedita possimus distinctio, dignissimos laborum! Animi deserunt at exercitationem, illo quae vero reprehenderit molestiae repudiandae quisquam natus assumenda!
+  </p>
+  <Link to="/"><button>Home</button></Link>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,17 @@
-import { Outlet, useNavigation, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
-
 const Home = () => {
-  const navigation=useNavigation();
+  const navigation = useNavigation();
   // const location=useLocation();
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
-    <Header></Header>
-    {
-      navigation.state === "loading"?
-      <p>Loading.....</p>:
-    <Outlet></Outlet>
-    }
-    <Footer></Footer>
+      <Header></Header>
+      {navigation.state === "loading" ? <p>Loading.....</p> : <Outlet></Outlet>}
+      <Footer></Footer>
     </div>
   );
 };
